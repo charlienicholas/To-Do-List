150cd55d56a189c873a8f03f7755d6b3a9e37cdd
charlienicholas <kangwibu129@gmail.com>
2026-05-14 19:12:35 +0700

    feat: menambahkan navbar

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+const btnTheme = document.getElementById("btn-theme");
+
+// Cek preferensi awal
+if (localStorage.getItem("theme") === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+// Event Listener Klik
+btnTheme.addEventListener("click", () => {
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  } else {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  }
+});
